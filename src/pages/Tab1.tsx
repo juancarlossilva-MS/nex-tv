b85d37a405d2c95561a458e450ef283c1b30a53e
Juan Carlos Silva <juancarlossilva@MacBook-Pro-de-Juan.local>
2021-10-14 13:10:16 -0300

    asdasdasd

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,26 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
+import ReactPlayer from "react-player";
+
+import { VideoPlayer } from '@ionic-native/video-player';
+import { useEffect } from 'react';
+
 
 const Tab1: React.FC = () => {
+
+
+  const openVideo = () =>{
+    const vp = VideoPlayer;
+        vp.play('https://www.facebook.com/251460212193971/videos/570252257397877').then(() => {
+          console.log('video finished');
+        }).catch(error => {
+          console.log(error);
+        });
+  }
+  
+
+
   return (
     <IonPage>
       <IonHeader>
@@ -11,12 +29,13 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <IonButton onClick={openVideo}>Open Video</IonButton>
+        <ReactPlayer
+          url={'https://www.facebook.com/reporterthalesbenicio/videos/1182944448880723/'}
+          controls
+          
+          playing
+        />
       </IonContent>
     </IonPage>
   );
