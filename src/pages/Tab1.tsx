d03b156d51f79232f42cb513a0b4751f7d42564e
juancarlossrp-ms <jjcarlos853@gmail.com>
2021-10-20 16:59:41 -0300

    moreonestep

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -10,33 +10,54 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation';
 import {firebaseConfig} from "../config/fire-config";
 
 import * as firebase from 'firebase/app';
+import { NavigationBar } from '@ionic-native/navigation-bar';
+import { AndroidFullScreen,AndroidSystemUiFlags } from '@ionic-native/android-full-screen';
 
 
 import {initializeFirestore,collection,doc,getDoc,onSnapshot} from 'firebase/firestore'
 
 const Tab1: React.FC = () => {
 
+  function successFunction()
+  {
+    alert("It worked!");
+  }
+
+    function errorFunction(error:any)
+    {
+      console.error(error);
+    }
+
+  function trace(value:any)
+  {
+  alert(value);
+  }
 
   useEffect(()=> {
+    
     if(window.navigator.onLine){
       
       initiaApp();
-
-  //   const doc = firestore.Firestore.collection('live').doc("on");
-  
-   /*
-   doc.onSnapshot(
-    (docSnapshot:any) => {
-  
-          alert(docSnapshot.exists)
-          alert(docSnapshot.data().link)
-    }, (err:any) => {
-      console.log(`Encountered error: ${err}`);
-    },
-  
-    );*/
-  }
+    }
+    AFS();
+   
   },[])
+
+  const AFS = async () => 
+{
+//Check if is immersive supported for instance
+await AndroidFullScreen.isImmersiveModeSupported().then((r: any) => {
+        alert(r);
+    }).catch((error)=>alert(error))
+//Set display with AndroidSystemUiFlags
+//in my case to full screen but there is a ton of options
+//As the android documentation shows if u want a fullscreen app you should use three
+//flags Immersive, Fullscreen, HideNavigation
+await AndroidFullScreen.setSystemUiVisibility(AndroidSystemUiFlags.Fullscreen)
+await AndroidFullScreen.setSystemUiVisibility(AndroidSystemUiFlags.Immersive)
+await AndroidFullScreen.setSystemUiVisibility(AndroidSystemUiFlags.HideNavigation)
+//example how to set a flag
+}
 
 
   const [link,setLink] = useState();
@@ -113,22 +134,15 @@ useEffect(()=>{
 
   },[state])
 
-    function chamaFullScreen(){
-      
-    screenfull.request(ref2.current.player.getInternalPlayer())
-  }
- 
-
-
+   
   return (
     <IonPage>
-      <IonButton  ref={ref}  onClick={chamaFullScreen}>FULL</IonButton>
         <ReactPlayer
           url={link}
           controls
           ref={ref2}
           playing
-          muted
+          
           fullscreen="true"
           width="100%"
           />
